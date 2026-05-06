e47483ef96612b72407fc298a02eac47cb93a06d
Raphaël Piédallu <raphael@citroen-ac4.fr>
2026-05-06 23:57:02 +0200

    fix: safe call to isUnread

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,6 +1,5 @@
 import { extend } from 'flarum/common/extend';
 import BasicsPage from 'flarum/admin/components/BasicsPage';
-import ColorPreviewInput from 'flarum/admin/components/ColorPreviewInput';
 
 app.initializers.add('fof-categories', () => {
   app.extensionData
@@ -62,17 +61,12 @@ app.initializers.add('fof-categories', () => {
       type: 'switch',
     })
 
-    .registerSetting(() => (
-      <div className="Form-group">
-        <label>{app.translator.trans('fof-categories.admin.labels.unread_color')}</label>
-        <ColorPreviewInput
-          value={app.data.settings['fof-categories.unread-color'] || '#e8a234'}
-	    onchange={(e) => {
-	      app.data.settings['fof-categories.unread-color'] = e.target.value;
-	    }}
-        />
-      </div>
-    ))
+    .registerSetting(() => <legend class="categories-legend">{app.translator.trans('fof-categories.admin.headings.unread')}</legend>)
+    .registerSetting({
+      setting: 'fof-categories.unread-color',
+      label: app.translator.trans('fof-categories.admin.labels.unread_color'),
+      type: 'color',
+    })
 
   extend(BasicsPage.prototype, 'homePageItems', (items) => {
     items.add('categories', {
